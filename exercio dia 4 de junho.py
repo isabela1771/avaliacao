704607410cdc3ff883fe1377896c398ffcd2b9b1
isabela1771 <isssaar13@gmail.com>
2025-06-04 14:21:24 -0300

    primeiro commit

---
+++ exercio dia 4 de junho.py
@@ -0,0 +1,23 @@
+class Pessoa:
+    def __init__(self, nome):
+        self.nome = nom
+
+    def apresentar (self):
+        print(f"Olá, meu nome é {self.nome}")
+
+class Professor(Pessoa):
+    def __init__(self, nome, departamento):
+        super().__init__(nome)
+        self.departamento = departamento #Aqui estamos passando um objeto
+
+    def apresentar(self):
+        print(f"Sou o Professor {self.nome}, do departamento de {self.departamento.nome}")
+
+class Departamento:
+    def __init__(self, nome):
+        self.nome = nome
+
+dep = Departamento("Computação")
+prof = Professor("Carlos", dep) #
+
+prof.apresentar()
